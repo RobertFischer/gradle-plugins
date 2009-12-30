e6f927f386934b30f4e398c6086b022b882d90ff
Robert Fischer <robert@robert-mbp.local>
2009-12-30 13:46:20 -0500

    Made logging and error a bit more explicit and useful

--- src/main/groovy/EnvPlugin.groovy
+++ src/main/groovy/EnvPlugin.groovy
@@ -12,12 +12,12 @@ class EnvPlugin extends SjitPlugin {
         envs.put("$k".replaceFirst(/^env\./, ""), v)
       }
     }
-    logger.info("Environment keys: ${envs.keySet() as SortedSet}")
+    logger.info("Environment keys: ${(envs.keySet() as List).sort()}")
     project.metaClass.env = Collections.unmodifiableMap(envs)
     project.metaClass.env = { k -> 
       k = k?.toString()
       if(!envs.containsKey(k)) {
-        throw new Exception("Could not find system environment variable: $k")
+        throw new Exception("Could not find system environment variable: $k (Use `env['$k']` instead of `env('$k')` to get null on not found)")
       }
       envs[k] 
     }
