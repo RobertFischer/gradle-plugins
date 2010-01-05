ff391d45a48873c6a7f333d6ecafaee88af930dc
Jeppe Nejsum Madsen <jeppe@jnmmbp.local>
2010-01-05 23:05:39 +0800

    Fixed NPE in Gradle 0.8 when no gems are installed

--- src/main/groovy/RunJRubyPlugin.groovy
+++ src/main/groovy/RunJRubyPlugin.groovy
@@ -57,7 +57,7 @@ class RunJRubyPlugin extends SjitPlugin {
         }
       }
 
-      return project.tryRelativePath(gemDir)
+     return gemDir == null ? null : project.tryRelativePath(gemDir)
     }
     project.metaClass.gemScript = { String gem ->
       def dir = project.gemHome(gem)
